b6176fe9629751277e5ab33cc51ca6c3b7dc5c35
PouncySilverkitten <struan@duncan-wilson.co.uk>
2018-06-22 20:55:38 +0100

    Restructure, add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(  name='doc2markdown',
+        version='1.00',
+        description='Converts Python docstrings to GitHub-flavoured markdown',
+        author='Pouncy Silverkitten',
+        author_email='pouncy.sk@gmail.com',
+        url='https://github.com/pouncysilverkitten/doc2markdown',
+        scripts=['doc2markdown/doc2markdown.py'],)
